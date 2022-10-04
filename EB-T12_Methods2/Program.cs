41cc3c68e328a8644a3a1360c2064db1ef76f2bb
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 03:54:13 +0300

    Method Example

---
+++ EB-T12_Methods2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodlarOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir Sayı Giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            int KareDeger = KareAl(sayi);
+
+            if (KareDeger > 25)
+            {
+                Console.WriteLine("Karesi 25'ten Büyüktür");
+            }
+            else
+            {
+                Console.WriteLine("Karesi 25'ten Küçüktür");
+
+            }
+            Console.ReadLine();
+
+
+        }
+        static int KareAl(int a)
+        {
+            return a * a;
+
+        }
+
+    }
+}
